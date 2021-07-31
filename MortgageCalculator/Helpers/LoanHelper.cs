6c03f3195e99726b6220e90de6179bc74911d642
Sankung Sillah <ssillah306@gmail.com>
2021-07-31 00:38:18 -0400

    completed all the math involved in our payment schedule

--- MortgageCalculator/Helpers/LoanHelper.cs
+++ MortgageCalculator/Helpers/LoanHelper.cs
@@ -11,13 +11,36 @@ namespace MortgageCalculator.Helpers
         public Loan GetPayments(Loan loan)
         {
             // calculate the monthly payments
-
+            loan.Payment = CalcPayment(loan.Amount, loan.Rate, loan.Term);
             //create loop for 1 to the term
-
+            var balance = loan.Amount;
+            var totalInterest = 0.0m;
+            var monthlyInterest = 0.0m;
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalcMonthlyRate(loan.Rate);
             // calculate a payment schedule
+            for (int month = 1; month <= loan.Term; month++)
+            {
+                monthlyInterest = CalcMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
 
+                LoanPayment loanPayment = new();
 
-            //push payments in the loan
+                loanPayment.Month = month;
+                loanPayment.Payment = loan.Payment;
+                loanPayment.MonthlyPrinciple = monthlyPrincipal;
+                loanPayment.MonthlyInterest = monthlyInterest;
+                loanPayment.TotalInterest = totalInterest;
+                loanPayment.Balance = balance;
+
+                loan.Payments.Add(loanPayment);
+
+            }
+
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest;
 
             // return loan to the view
 
@@ -39,6 +62,11 @@ namespace MortgageCalculator.Helpers
         private decimal CalcMonthlyRate(decimal rate)
         {
             return rate / 1200;
+        }
+
+        private decimal CalcMonthlyInterest(decimal balance, decimal montlyRate)
+        {
+            return balance * montlyRate;
         }
     }
 }
